49c2c4c3bdc89689b5a6e47e3740ffedba733ebd
Interitio <cona@thewisewolf.dev>
2024-08-31 15:31:16 +1000

    fix: Add channel link data.

--- data/schema.sql
+++ data/schema.sql
@@ -1404,6 +1404,27 @@ CREATE TABLE stream_alerts(
 
 
 -- }}}
+-- Channel Linker {{{
+
+CREATE TABLE links(
+  linkid SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE channel_webhooks(
+  channelid BIGINT PRIMARY KEY,
+  webhookid BIGINT NOT NULL,
+  token TEXT NOT NULL
+);
+
+CREATE TABLE channel_links(
+  linkid INTEGER NOT NULL REFERENCES links (linkid) ON DELETE CASCADE,
+  channelid BIGINT NOT NULL REFERENCES channel_webhooks (channelid) ON DELETE CASCADE,
+  PRIMARY KEY (linkid, channelid)
+);
+
+
+-- }}}
 
 
 -- Nowlist {{{
